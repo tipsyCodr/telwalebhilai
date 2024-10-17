daca5b73768098b3f646308a3e3d1462a8996898
Hemant Kumar <itshemant566@gmail.com>
2024-10-17 20:38:06 +0530

    sd

--- licenses.php
+++ licenses.php
@@ -35,7 +35,7 @@
             <div class="card">
                 <object data="assets/pdf/license.pdf" type="application/pdf" frameborder="0" width="100%" height="600px"
                     style="padding: 20px;">
-                    <embed src="https://drive.google.com/file/d/1CRFdbp6uBDE-YKJFaqRm4uy9Z4wgMS7H/preview?usp=sharing"
+                    <embed src="https://drive.google.com/file/d/1B65AMQ2IELIkQzIGtznpa2sAYMlXBiW-/preview?usp=sharing"
                         width="100%" height="600px" />
                 </object>
             </div>
