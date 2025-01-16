7f16353c5a0b58c16446ebfef1f9928a3f2644a9
Hemant Kumar <itshemant566@gmail.com>
2025-01-16 12:56:37 +0530

    s

--- partials/navbar.php
+++ partials/navbar.php
@@ -64,7 +64,7 @@
 
 </div>
  <!-- Floating button  -->
- <a href="https://wa.me/919131923249" class="bg-green-500 hover:bg-green-700 fixed  text-white font-bold py-2 px-4 rounded-full" style="bottom:10px;left:20px;z-index:10;animation: slideInFromBottom 1s ease-in-out forwards;" >
+ <a href="https://wa.me/919131923249" class="bg-green-500 hover:bg-green-700 fixed  text-white font-bold py-2 px-4 rounded-full" style="bottom:10px;right:20px;z-index:10;animation: slideInFromBottom 1s ease-in-out forwards;" >
        <img src="./assets/img/whatsapp.png" class="w-full h-full" style="width: 60px;height: 60px;">
     </a>
 
